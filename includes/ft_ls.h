a1e41240d54a24eb1d894a526ceb78187cb22976
alexSolohin <solohinalex1@gmail.com>
2020-04-08 13:14:48 +0300

    add ft_ls_dir

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 17:54:07 by rmaxima           #+#    #+#             */
-/*   Updated: 2020/04/06 15:08:32 by user             ###   ########.fr       */
+/*   Updated: 2020/04/08 13:12:37 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,13 @@ typedef struct      s_ls
 {
     int             flag;			//флаг
 	char			chmod[10];		//права доступа
-	char			time[17];		//время DD MM
+	char			*time;		//время DD MM
 	char			*user_name;		//имя пользовтеля
 	char			*group_name;	//имя групы
 	int				byte_size;		//размер в байтах
 	int				links;			//количество файлов внутри
     char            *name;			// имя файла или директории
+	char			*name_dir;
     struct s_ls     *next;
 }                   t_ls;
 
@@ -35,17 +36,10 @@ typedef struct		s_type
 	int				flag;
 }					t_type;
 
-typedef struct		s_dir
-{
-	char			*name;
-	int				flag;
-	t_ls			*ls;
-	struct s_dir	*next;
-}					t_dir;
-
 void	print(t_ls *ls);
 void	ft_ls_l(t_ls *ls);
 void	print_error(char *av);
 t_ls	*sort_list(t_ls *ls);
+void	init_struct(t_ls *ls);
 
 #endif
