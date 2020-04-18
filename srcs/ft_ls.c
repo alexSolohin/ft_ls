6914b58f4db3670324f29353b82a52f4633269b5
alexSolohin <solohinalex1@gmail.com>
2020-04-18 21:27:41 +0300

    changes stat()

--- srcs/ft_ls.c
+++ srcs/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/17 11:55:26 by alex              #+#    #+#             */
-/*   Updated: 2020/04/18 17:14:49 by user             ###   ########.fr       */
+/*   Updated: 2020/04/18 17:24:47 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,25 +48,39 @@ int     main(int ac, char **av)
 	t_ls	*ls;
 	t_ls	*ptr;
 	char	*ptri;
-
+	struct	stat	file_stat;
 
 	ls = malloc(sizeof(t_ls));
 	dir = opendir(av[1]);
 	if (!dir)
 		exit(0);
-
 	ptri = malloc(sizeof(char) * 100);
 	ptri = ft_strcpy(ptri, av[1]);
 	ptri[ft_strlen(av[1])] = '/';
 	while ((entry = readdir(dir)) != NULL)
 	{
+
 		ptr = malloc(sizeof(t_ls));
 		ptr->name = ft_strdup(ptri);
 		ptr->name = ft_strcat(ptr->name, entry->d_name);
 		printf("%s\n", ptr->name);
+		if (stat(entry->d_name, &file_stat) == -1)
+			printf("bad idea\n");
+		printf( (S_ISDIR(file_stat.st_mode)) ? "d" : "-");
+		printf( (file_stat.st_mode & S_IRUSR) ? "r" : "-");
+		printf( (file_stat.st_mode & S_IWUSR) ? "w" : "-");
+		printf( (file_stat.st_mode & S_IXUSR) ? "x" : "-");
+		printf( (file_stat.st_mode & S_IRGRP) ? "r" : "-");
+		printf( (file_stat.st_mode & S_IWGRP) ? "w" : "-");
+		printf( (file_stat.st_mode & S_IXGRP) ? "x" : "-");
+		printf( (file_stat.st_mode & S_IROTH) ? "r" : "-");
+		printf( (file_stat.st_mode & S_IWOTH) ? "w" : "-");
+		printf( (file_stat.st_mode & S_IXOTH) ? "x" : "-");
+		printf("\n\n");
+
+
 		// ptr->next = ls;
 		// ls = ptr;
-		// ft_bzero(ptr->name, (ft_strlen(ptr->name) - ft_strlen(ptri)));
 		free(ptr->name);
 	}
 	// while (ls->next != NULL)
