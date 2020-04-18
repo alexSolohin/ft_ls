207a5b6be595cf0cddd6f1b31bc5273838f7c781
alexSolohin <solohinalex1@gmail.com>
2020-04-18 17:16:29 +0300

    add stat()

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 17:54:07 by rmaxima           #+#    #+#             */
-/*   Updated: 2020/04/15 15:39:45 by user             ###   ########.fr       */
+/*   Updated: 2020/04/18 15:39:26 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,7 @@ typedef struct      s_ls
 {
 	t_flag			flag;			//флаг
 	char			chmod[10];		//права доступа
-	char			*time;		//время DD MM
+	char			*time;			//время DD MM
 	char			*user_name;		//имя пользовтеля
 	char			*group_name;	//имя групы
 	int				byte_size;		//размер в байтах
@@ -105,13 +105,12 @@ static t_lopt 	g_lopt[] = {
 void		print(t_ls *ls);
 void		ft_ls_l(t_ls *ls);
 void		print_error(char *av);
-void		init_struct(t_ls *ls, char *str);
+void		init_struct(char *str, t_ls *ls);
 t_opt 		*set_start_opt_val(t_opt *opt);
 int			ft_getopt(t_input inpt, t_opt *opt);
 int 		ft_getopt_long(t_input data, t_opt **opt, t_lopt *lopt, int *lind);
 //int 		get_num_of_array_index(t_input input);
 void 		reset_flags(t_flag *flag);
 void 		collect_flags(t_flag *flag, int ac, char ***av);
-
 
 #endif
