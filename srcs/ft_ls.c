218f2147f1236dd852a0caa2da8f4de3168104fb
alexSolohin <solohinalex1@gmail.com>
2020-04-22 11:26:46 +0300

    add total

--- srcs/ft_ls.c
+++ srcs/ft_ls.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ls.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/17 11:55:26 by alex              #+#    #+#             */
-/*   Updated: 2020/04/23 17:21:42 by user             ###   ########.fr       */
+/*   Updated: 2020/04/20 15:26:08 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,84 +23,6 @@
 //	opendir(../dir)
 //		while (entry = readdir())
 
-int		sort_alphabet(char *a, char *b)
-{
-	int i;
-
-	i = 0;
-	while (b[i])
-	{
-		if (a[i] > b[i])
-			return (1);
-		else
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-t_ls	*sort_list(t_ls *ls)
-{
-//	char	*tmp;
-	int		type_a;
-	t_ls	*ptr;
-	t_ls	*tmp;
-
-// struct list * sort( struct list *root )
-// {
-//     struct list *new_root = NULL;
-
-//     while ( root != NULL )
-//     {
-//         struct list *node = root;
-//         root = root->next;
-
-//         if ( new_root == NULL || node->data < new_root->data )
-//         {
-//             node->next = new_root;
-//             new_root = node;
-//         }
-//         else
-//         {
-//             struct list *current = new_root;
-//             while ( current->next != NULL && !( node->data < current->next->data ) )
-//             {
-//                   current = current->next;
-//             }
-
-//             node->next = current->next;
-//             current->next = node;
-//         }
-//     }
-
-//     return new_root;
-// }
-
-	ptr = NULL;
-	type_a = 0;
-	while (ls != NULL)
-	{
-		// if (ls->name[0] == '.' && type_a != 1)
-		tmp = ls;
-		ls = ls->next;
-		if (ptr == NULL || sort_alphabet(tmp->name, ptr->name))
-		{
-			tmp->next = ptr;
-			ptr = tmp;
-		}
-		else
-		{
-			t_ls *current = ptr;
-			while (current->next != NULL && !sort_alphabet(tmp->name, current->next->name))
-			{
-				current = current->next;
-			}
-			tmp->next = current->next;
-			current->next = tmp;
-		}
-	}
-	return (ptr);
-}
 
 void	ft_ls_dir(char *av)
 {
@@ -109,28 +31,31 @@ void	ft_ls_dir(char *av)
 	char			*path;
 	t_ls			*ls;
 	t_ls			*ptr;
+	int				total;
 
 	if (!(ls = malloc(sizeof(t_ls))))
 		exit(0);
 	dir = opendir(av);
 	if (!dir)
 		exit(0);
-	if (!(path = malloc(sizeof(char) * 255)))
+	if (!(path = malloc(sizeof(char))))
 		exit(0);
 	path = ft_strcpy(path, av);
 	path[ft_strlen(av)] = '/';
+	total = 0;
 	while ((entry = readdir(dir)) != NULL)
 	{
 		ptr = malloc(sizeof(t_ls));
 		ptr->name = entry->d_name;
 		ptr->path = ft_strdup(path);
 		ptr->path = ft_strcat(ptr->path, entry->d_name);
-		init_struct(ptr, av);
+		init_struct(ptr);
+		total += ptr->byte_size / 1000;
 		ptr->next = ls;
 		ls = ptr;
 		free(ptr->path);
 	}
-	// sort_list(ls);
+	printf("total %d\n", total);
 	while (ls->next != NULL)
 	{
 		int i = 0;
