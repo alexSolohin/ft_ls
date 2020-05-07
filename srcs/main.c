9dc2babd0963a7eaab3c2cdff17fd4e152a2fca5
Jpasty <nebalyi93@gmail.com>
2020-05-07 19:27:04 +0300

    07/05/20

--- srcs/main.c
+++ srcs/main.c
@@ -1,21 +1,64 @@
 #include "ft_ls.h"
 #include <stdio.h>
 
-
-int			main(int ac, char **av)
+void		add_ls_node(t_ls *entry, t_ls **ls)
 {
-	t_ls	*ls;
-	ls = (t_ls*)malloc(sizeof(t_ls));
+	t_ls	*tmp;
 
-	reset_flags(&(ls->flag));
-	if (ac >= 2)
+	if (!(tmp = (t_ls *)malloc(sizeof(t_ls))))
 	{
-		collect_flags(&(ls->flag), ac, &av);
-		ft_printf("%s", *av);
-		ft_ls_dir(*av);
+		strerror(errno);                //добавить обработку исключений
+		return;
 	}
+	if(entry && ls)
+	{
+		*tmp = *entry;
+		tmp->next = *ls;
+		*ls = tmp;
+	}
+}
+
+int 			input_processing(t_flag flag, int ac, char **av)
+{
+	t_ls 		*fls;
+	t_ls 		*dls;
+	t_ls 		entry;
+	int i;
+
+	fls = NULL;
+	dls = NULL;
+	i = 0;
+	while (i < ac)
+	{
+		if (!init_struct(&entry, flag, av[i++]))
+			continue;
+		if (!entry.flag.d && entry.d_mode)
+			add_ls_node(&entry, &dls);
+		else
+			add_ls_node(&entry, &fls);
+	}
+
+
+}
+
+int				main(int ac, char **av)
+{
+
+	t_flag		flag;
+	collect_flags(&flag, &ac, &av);
+
+	if (ac)
+		input_processing(flag, ac, av);
 	else
-		//ft_ls_dir(".");
-		ft_printf("---");
-	return (0);
+		ft_ls_dir(CURR_DIR);
+
+			//init_struct(ls, av[i]);
+
+			//if (!dls->flag.d && dls->chmod[0] == 'd')
+
+			//else
+				//	ndir;
+				//	nifle;
+
+
 }
